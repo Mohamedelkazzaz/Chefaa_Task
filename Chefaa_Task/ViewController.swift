9b2f5d5b24501d5f504f77085113c80a4e546b0e
Mohamed Elkazzaz <m.elkazzaz96@gmail.com>
2023-12-25 23:36:05 +0200

    Setup Task

---
+++ Chefaa_Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chefaa_Task
+//
+//  Created by Mohamed Elkazzaz on 25/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
